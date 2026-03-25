18bec8bec798940a8b4872f3ca4d4f53838742eb
Yiduu <tamiruyididiya854@gmail.com>
2026-03-25 21:14:30 +0300

    Prepare for Render deployment

--- ccms/settings.py
+++ ccms/settings.py
@@ -7,12 +7,12 @@ import dj_database_url
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / '.env')
 
 # Security
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
-
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
